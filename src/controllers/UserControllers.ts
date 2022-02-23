55530475f4c27f020b2ef9ea9db8f979ad8174ae
Cecilia Cacais <ceci.cacais@gmail.com>
2022-02-23 15:00:54 -0300

    Cria token do usuario

--- src/controllers/UserControllers.ts
+++ src/controllers/UserControllers.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express';
 import createUserService from '../service/UserServices';
+import tokenCreate from '../token/Token';
 import StatusCode from '../enums/statusCode';
 
 const createUserController = async (req: Request, res: Response) => {
   const user = req.body;
   const newUser = await createUserService(user);
-  res.status(StatusCode.CREATED).json(newUser);
+  const userToken = await tokenCreate(newUser.id, newUser.username);
+  res.status(StatusCode.CREATED).json({ token: userToken });
 };
 
 export default createUserController;
---
+++ src/token/Token.ts
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
+const jwtPassword = String(process.env.Jwt_SECRET);
+
+const jwtOptions: jwt.SignOptions = {
+  algorithm: 'HS256',
+  expiresIn: '10h',
+};
+
+const tokenCreate = async (id: number, username: string) => {
+  const payload = {
+    id,
+    username,
+  };
+  const token = jwt.sign(payload, jwtPassword, jwtOptions);
+  return token;
+};
+
+export default tokenCreate;
